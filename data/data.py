f492ead73ff061cc1405cc5dfc3c6ece0143e828
lambotik <lambotik@users.noreply.github.com>
2023-10-08 08:22:15 +0000

    deploy: 38b28b7555b8b0c942ce2e8bb4b9d91a65a5d7fd

--- data/data.py
+++
@@ -1,9 +0,0 @@
-from dataclasses import dataclass
-
-
-@dataclass
-class Person:
-    full_name: str = None
-    email: str = None
-    current_address: str = None
-    permanent_address: str = None
